abc68194b65385291a097680bf5d1709f5fff0d6
Kwabena Boakye <kwaboakye93@gmail.com>
2023-03-10 15:26:37 +0000

    Build my own calculator

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    from sys import argv
+    from calculator_1 import add, sub, mul, div
+
+
+    num_of_args = len(argv) - 1
+
+    if num_of_args != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+
+    operator = argv[2]
+
+    operators = "+-/*"
+
+    a = int(argv[1])
+    b = int(argv[3])
+    result = 0
+
+    match operator:
+        case "+":
+            result = add(a, b)
+        case "-":
+            result = sub(a, b)
+        case "*":
+            result = mul(a, b)
+        case "/":
+            result = div(a, b)
+        case _:
+            print("Unknown operator. Available operators: +, -, * and /")
+            exit(1)
+
+    print("{:d} {:s} {:d} = {:d}".format(a, operator, b, result))
