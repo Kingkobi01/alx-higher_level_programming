c74b13739f989470c3f4b983c988aea474de262d
Kwabena Boakye <kwaboakye93@gmail.com>
2023-03-12 14:49:44 +0000

    Can you C me now?

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,11 +2,8 @@
 
 
 def no_c(my_string):
-    if my_string:
-        new_string = ""
-        for i in my_string:
-            if i.lower() != "c":
-                new_string += i
-        return new_string
-    else:
-        return None
+    new_string = ""
+    for i in my_string:
+        if i.lower() != "c":
+            new_string += i
+    return new_string
