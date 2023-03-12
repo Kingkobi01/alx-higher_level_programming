8c4521a51782c437ef402712d24f73a1fe12fcc7
Kwabena Boakye <kwaboakye93@gmail.com>
2023-03-12 15:53:21 +0000

    Tuples addition

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -2,14 +2,13 @@
 
 
 def add_tuple(tuple_a=(), tuple_b=()):
-    if tuple_a:
-        tuple_a = clean_tuple(tuple_a)
-        tuple_b = clean_tuple(tuple_b)
+    tuple_a = clean_tuple(tuple_a)
+    tuple_b = clean_tuple(tuple_b)
 
-        first = tuple_a[0] + tuple_b[0]
-        last = tuple_a[1] + tuple_b[1]
+    sum_first = tuple_a[0] + tuple_b[0]
+    sum_last = tuple_a[1] + tuple_b[1]
 
-        return (first, last)
+    return (sum_first, sum_last)
 
 
 def clean_tuple(tple=()):
