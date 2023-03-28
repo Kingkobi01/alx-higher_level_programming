57201e934552b9ddadb4fecbe67577afdb1fde2f
Kwabena Boakye <kwaboakye93@gmail.com>
2023-03-28 23:02:20 +0000

    Area of a square

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+# 0-square.py by Kwabena Boakye
+"""This class is a blueprint for making square instances."""
+
+
+class Square:
+    """Defines a square"""
+
+    def __init__(self, size=0) -> None:
+        """Initializes a square and validates it
+        Args:
+            size: represents the size of the square defined
+        Raises:
+            TypeError: if size is not an integer
+            ValueError: if size is less than 0"""
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
