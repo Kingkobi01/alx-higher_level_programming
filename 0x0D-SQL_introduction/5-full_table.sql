f1efb2b716c115edf87d0b9e878d915e0919ccf4
Kwabena Boakye <kwaboakye93@gmail.com>
2023-05-16 07:12:56 -0700

    Update comment syntax

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,5 +1,5 @@
- -- Prints the full description of tthe table first_table.
- -- DESCRIBE or EXPLAIN not allowed
+-- Prints the full description of tthe table first_table.
+-- DESCRIBE or EXPLAIN not allowed
 
 SHOW CREATE TABLE first_table;
 
