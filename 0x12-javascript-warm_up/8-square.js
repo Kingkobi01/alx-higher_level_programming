ed4c2d97980af24d906fb6f89b408225d81b50b7
Kwabena Boakye <kwaboakye93@gmail.com>
2023-06-11 18:26:31 +0000

    Square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,11 +1,17 @@
 #!/usr/bin/node
+const x = process.argv[2];
 
-const size = parseInt(process.argv[2]);
-
-if (!size) {
+if (!parseInt(x)) {
   console.log('Missing size');
 } else {
-  for (let s1 = size; s1 > 0; s1--) {
-    console.log('x'.repeat(size));
+  for (let i = 0; i < x; i++) {
+    let y = 0;
+    let myVar = '';
+
+    while (y < x) {
+      myVar = myVar + 'X';
+      y++;
+    }
+    console.log(myVar);
   }
 }
