5d4f97b789abdee6fc8f2c60e02bc0be68996ea2
Kwabena Boakye <kwaboakye93@gmail.com>
2023-06-28 16:24:52 +0000

    cURL Method

---
+++ 0x10-python-network_0/1-body.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+#Takes in a URL, sends a GET request to the URL, and displays the body of the response
+
+curl -sL "$1" -X GET -D ./header -o ./output; if grep -q "200 OK" ./header; then cat ./output; fi
