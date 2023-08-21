30e36313b5199ba70d8fcb383fb93a45d0bc84d7
Kwabena Boakye <kwaboakye93@gmail.com>
2023-08-21 13:54:01 +0000

    Base class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,10 +1,22 @@
 #!/usr/bin/env python3
 
 
+
+"""
+
+A model that contains a Base class to manage
+the id attribute of all classes that extend
+from Base and avoid duplicate the same code.
+
+"""
+
 class Base:
     __nb_objects = 0
 
     def __init__(self, id=None) -> None:
+        """
+        ...
+        """
         if id:
             self.id = id
         else:
