2aba73846c14ad50fb81de626ec6004ef3a4a575
Kwabena Boakye <kwaboakye93@gmail.com>
2023-08-21 14:50:05 +0000

    Almost a circle

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,64 +1,140 @@
-#!/usr/bin/env python3
-
-"""
-A module that test differents behaviors
-of the Base class
-"""
+#!/usr/bin/python3
 import unittest
-import pep8
 from models.base import Base
+from models.square import Square
+import json
+import inspect
 
-class TestModel(unittest.TestCase):
-
-    def setUp(self) -> None:
-        self.b0 = Base()
-        self.b01 = Base()
-        self.b02 = Base()
-        self.b2 = Base(4)
-        self.b3 = Base(12)
-        self.b4 = Base(-1)
-        self.b5 = Base(-12)
+'''
+    Creating test cases for the base module
+'''
 
 
-    def test_pep8_base(self):
+class test_base(unittest.TestCase):
+    '''
+        Testing base
+    '''
+    def test_id_none(self):
+        '''
+            Sending no id
+        '''
+        b = Base()
+        self.assertEqual(1, b.id)
+
+    def test_id(self):
+        '''
+            Sending a valid id
+        '''
+        b = Base(50)
+        self.assertEqual(50, b.id)
+
+    def test_id_zero(self):
+        '''
+            Sending an id 0
+        '''
+        b = Base(0)
+        self.assertEqual(0, b.id)
+
+    def test_id_negative(self):
+        '''
+            Sending a negative id
+        '''
+        b = Base(-20)
+        self.assertEqual(-20, b.id)
+
+    def test_id_string(self):
+        '''
+            Sending an id that is not an int
+        '''
+        b = Base("Betty")
+        self.assertEqual("Betty", b.id)
+
+    def test_id_list(self):
+        '''
+            Sending an id that is not an int
+        '''
+        b = Base([1, 2, 3])
+        self.assertEqual([1, 2, 3], b.id)
+
+    def test_id_dict(self):
+        '''
+            Sending an id that is not an int
+        '''
+        b = Base({"id": 109})
+        self.assertEqual({"id": 109}, b.id)
+
+    def test_id_tuple(self):
+        '''
+            Sending an id that is not an int
+        '''
+        b = Base((8,))
+        self.assertEqual((8,), b.id)
+
+    def test_to_json_type(self):
+        '''
+            Testing the json string
+        '''
+        sq = Square(1)
+        json_dict = sq.to_dictionary()
+        json_string = Base.to_json_string([json_dict])
+        self.assertEqual(type(json_string), str)
+
+    def test_to_json_value(self):
+        '''
+            Testing the json string
+        '''
+        sq = Square(1, 0, 0, 609)
+        json_dict = sq.to_dictionary()
+        json_string = Base.to_json_string([json_dict])
+        self.assertEqual(json.loads(json_string),
+                         [{"id": 609, "y": 0, "size": 1, "x": 0}])
+
+    def test_to_json_None(self):
+        '''
+            Testing the json string
+        '''
+        sq = Square(1, 0, 0, 609)
+        json_dict = sq.to_dictionary()
+        json_string = Base.to_json_string(None)
+        self.assertEqual(json_string, "[]")
+
+    def test_to_json_Empty(self):
+        '''
+            Testing the json string
+        '''
+        sq = Square(1, 0, 0, 609)
+        json_dict = sq.to_dictionary()
+        json_string = Base.to_json_string([])
+        self.assertEqual(json_string, "[]")
+
+
+class TestSquare(unittest.TestCase):
+    """
+    class for testing Base class' methods
+    """
+
+    @classmethod
+    def setUpClass(cls):
         """
-        Test that checks PEP8
+        Set up class method for the doc tests
         """
-        syntax = pep8.StyleGuide(quit=True)
-        check = syntax.check_files(['models/base.py'])
-        self.assertEqual(
-            check.total_errors, 0,
-            "Found code style errors (and warnings)."
-        )
+        cls.setup = inspect.getmembers(Base, inspect.isfunction)
 
-
-
-    def test_base__positive_id(self):
+    def test_module_docstring(self):
         """
-        Tests for normal positive base ids
+        Tests if module docstring documentation exist
         """
-        self.assertEqual(self.b2.id, 4)
-        self.assertEqual(self.b3.id, 12)
+        self.assertTrue(len(Base.__doc__) >= 1)
 
-    def test_base__negative_id(self):
+    def test_class_docstring(self):
         """
-        Tests for normal negative base ids
+        Tests if class docstring documentation exist
         """
-        self.assertEqual(self.b4.id, -1)
-        self.assertEqual(self.b5.id, -12)
+        self.assertTrue(len(Base.__doc__) >= 1)
 
-    def test_base__default_id(self):
+    def test_func_docstrings(self):
         """
-        Tests for normal default base ids
+        Tests if methods docstring documntation exist
         """
-        self.assertEqual(self.b0.id, 1)
-        self.assertEqual(self.b01.id, 2)
-        self.assertEqual(self.b02.id, 3)
-
-
-def main():
-    unittest.main()
-
-
-if __name__ == "__main__":
-    main()
+        for func in self.setup:
+            self.assertTrue(len(func[1].__doc__) >= 1)
