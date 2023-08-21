32d844315f4395ab2ec32cb5799dfde4b337f394
Kwabena Boakye <kwaboakye93@gmail.com>
2023-08-21 13:42:21 +0000

    Base class

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -0,0 +1,24 @@
+import unittest
+from models.base import Base
+
+class TestModel(unittest.TestCase):
+
+    def setUp(self) -> None:
+        b1 = Base()
+        b2 = Base(12)
+        b3 = Base()
+
+
+
+    def test_model_id(self):
+        self.assertEqual(self.b1.id, 0)
+        self.assertEqual(self.b2.id, 12)
+        self.assertEqual(self.b3.id, 1)
+
+
+def main():
+    unittest.main()
+
+
+if __name__ == "__main__":
+    main()
