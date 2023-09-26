b3562557d6940b7b2189db081d754efab4b592e0
Kwabena Boakye <kwaboakye93@gmail.com>
2023-09-26 18:23:30 +0000

    Loripsum

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+const fs = require("fs");
+const request = require("request");
+
+async function requestStore(uri, destination) {
+  request(uri, function (error, response, body) {
+    if (error) {
+      console.log(error);
+    }
+    fs.writeFileSync(destination, body, "utf-8", function (err) {
+      console.log(err);
+    });
+  });
+}
+
+requestStore(process.argv[2], process.argv[3]);
